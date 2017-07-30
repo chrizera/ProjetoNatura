a21bb7b1d176bc5edc870599c58f5141a11a2f7e
chrizera <chris-perrone@hotmail.com>
2017-07-30 19:05:58 -0300

    Correcao do codigo da table view

--- ProjetoNatura/ProjetoNatura/ListaProdutoTableViewController.swift
+++ ProjetoNatura/ProjetoNatura/ListaProdutoTableViewController.swift
@@ -37,15 +37,15 @@ class ListaProdutoTableViewController: UITableViewController {
         return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProdutoCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
